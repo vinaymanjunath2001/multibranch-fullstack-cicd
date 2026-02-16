d5a1f778176432080176393bf28a00f1fe083a25
vinaymanjunath2001 <vinaymanjunath2004@gmail.com>
2026-02-17 02:36:22 +0530

    frontend UI done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -55,7 +55,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>User Submit Form</h1>
+      <h1>User Registration Form</h1>
       <input name="input-parameter" onChange={handleChange} />
       <br />
       <br />
