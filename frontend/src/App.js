dcdad5c84b2ec7ceb8a8c4de9d61f8249fba6992
vinaymanjunath2001 <vinaymanjunath2004@gmail.com>
2026-02-21 22:59:33 +0530

    app.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -55,7 +55,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>User Registration Form</h1>
+      <h1>User Registration Form dev_env</h1>
       <input name="input-parameter" onChange={handleChange} />
       <br />
       <br />
